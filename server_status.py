b17b71163c9dce7c58e87d06b8e2998ae51b03fb
Chris <chrismjboyle@gmail.com>
2019-10-29 15:19:39 +0000

    Shows per-user RAM usage too

--- server_status.py
+++ server_status.py
@@ -11,6 +11,7 @@ def get_server_status(
     server_output = runsh("ssh chris@srv \"top -b -n 1\"")[:-1]
     user_commands = defaultdict(list)
     user_proc_cpu = defaultdict(lambda: defaultdict(float))
+    user_proc_ram = defaultdict(lambda: defaultdict(float))
     tot_cpu = 0.0
     tot_mem = 0.0
     ncpus = 112
@@ -30,29 +31,31 @@ def get_server_status(
             continue
         user_commands[user].append(cmd)
         user_proc_cpu[user][cmd] += cpu/ncpus
+        user_proc_ram[user][cmd] += mem
     
     avload = tot_cpu / ncpus
     table = [ 
         [f'TOT. RAM: {tot_mem:.0f}%', f'CPU: {avload:.0f}%'], 
-        ['USER', '#PROCS', 'CPU', 'HIGHLIGHTS'] 
+        ['USER', '#PROCS', 'CPU', 'RAM', 'HIGHLIGHTS'] 
     ]
     for user, procs in user_commands.items():
         proc_count = len(procs)
         unique_procs = list(set(procs))
 
-        proc_count_cpu = [(uniq, procs.count(uniq), user_proc_cpu[user][uniq]) for uniq in unique_procs]
+        proc_count_cpu = [(uniq, procs.count(uniq), user_proc_cpu[user][uniq], user_proc_ram[user][uniq]) for uniq in unique_procs]
 
         # sort processes in descending order by CPU usage
         proc_count_cpu = list(reversed(sorted(proc_count_cpu, key=lambda r: r[2])))
 
-        # get total user CPU usage
+        # get total user CPU and RAM usage
         user_cpu = sum([pcc[2] for pcc in proc_count_cpu])
+        user_ram = sum([pcc[3] for pcc in proc_count_cpu])
 
         # trim out small usage (\leq .5%) processes
         proc_count_cpu = list(filter(lambda r: r[2] > trunc_perc, proc_count_cpu))
 
-        table.append([f'{user}', f'{len(procs)}', f'{user_cpu:.1f}%', ' ['])
-        for proc, count, __ in proc_count_cpu:
+        table.append([f'{user}', f'{len(procs)}', f'{user_cpu:.1f}%', f'{user_ram:.1f}', ' ['])
+        for proc, count, __, __ in proc_count_cpu:
             if count > 1:
                 table[-1][-1] += f'{count}×'
             table[-1][-1] += proc
