32da288453313b7f410e10de82840b526f0cc3ab
Chris <chrismjboyle@gmail.com>
2019-11-04 09:14:53 +0000

    Had the admin check backwards
    Oops

--- ircbot/admin.py
+++ ircbot/admin.py
@@ -10,21 +10,21 @@ def is_admin(bot, from_nick, username, hostname):
 
 def seppuku(bot, rest_of_message, from_nick, username, hostname, *args):
     if is_admin(bot, from_nick, username, hostname):
-        dislike_user(bot, from_nick)
-        chastise(bot)
-        return 0
-    else:
         bot.send_msg(f'BYE {random.choice(bot.phrase_book["people"])}.')
         bot.send_cmd(f'QUIT SEPPUKU\n')
         exit(1)
+    else:
+        dislike_user(bot, from_nick)
+        chastise(bot)
+        return 0
 
 
 def restart(bot, rest_of_message, from_nick, username, hostname, *args):
     if is_admin(bot, from_nick, username, hostname):
-        dislike_user(bot, from_nick)
-        chastise(bot)
-        return 0
-    else:
         bot.send_msg('I WILL BE RIGHT BACK')
         bot.send_cmd(f'QUIT RESTARTING\n')
         exit(0)
+    else:
+        dislike_user(bot, from_nick)
+        chastise(bot)
+        return 0
